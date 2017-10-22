d5f9456bfa754e155920df685e892d716c1524d5
Jeff <admin@etisdew.website>
2017-10-21 21:42:44 -0700

    Update recognizer.js

--- dist/recognizer.js
+++ dist/recognizer.js
@@ -3,7 +3,7 @@ function setup() {
     let recording = new p5.SpeechRec('en-US');
     recording.onResult = showResult;
 
-    let continuous = false;
+    let continuous = true;
     let iterim = true;
   
     recording.start(continuous, iterim);
